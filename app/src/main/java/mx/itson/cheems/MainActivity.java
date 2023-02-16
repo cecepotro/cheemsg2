922ccd57025e1785a47b54f9cdaf078e6a5dbfde
Jesús Antonio Ceceña <jantonio.cecena@gmail.com>
2023-02-16 16:23:34 -0700

    Versión inicial del proyecto

---
+++ app/src/main/java/mx/itson/cheems/MainActivity.java
@@ -0,0 +1,25 @@
+package mx.itson.cheems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iniciar();
+    }
+
+    public void iniciar(){
+        (findViewById(R.id.opcion1)).setBackgroundResource(R.drawable.icon_pregunta);
+        (findViewById(R.id.opcion2)).setBackgroundResource(R.drawable.icon_pregunta);
+        (findViewById(R.id.opcion3)).setBackgroundResource(R.drawable.icon_pregunta);
+        (findViewById(R.id.opcion4)).setBackgroundResource(R.drawable.icon_pregunta);
+        (findViewById(R.id.opcion5)).setBackgroundResource(R.drawable.icon_pregunta);
+        (findViewById(R.id.opcion6)).setBackgroundResource(R.drawable.icon_pregunta);
+    }
+}
