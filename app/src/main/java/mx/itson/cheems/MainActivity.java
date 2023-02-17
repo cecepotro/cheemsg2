9800885cf0b50f5d3bb00e41a36df398d6652a8e
Jesús Antonio Ceceña <jantonio.cecena@gmail.com>
2023-02-17 16:50:00 -0700

    Se agregó método para cambiar entre imágenes acorde a la selección del usuario

--- app/src/main/java/mx/itson/cheems/MainActivity.java
+++ app/src/main/java/mx/itson/cheems/MainActivity.java
@@ -3,8 +3,16 @@ package mx.itson.cheems;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public int ubicacion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iniciar();
+
+        Button btnIniciar = (Button)findViewById(R.id.btnReiniciar);
+        btnIniciar.setOnClickListener(this);
+
+        for(int i=1;i<=6;i++){
+            ImageButton btnSeleccion = (ImageButton)  findViewById((
+                    getResources().getIdentifier("opcion"+i, "id", this.getPackageName())));
+       btnSeleccion.setOnClickListener(this);
+        }
+
     }
 
     public void iniciar(){
@@ -21,5 +39,63 @@ public class MainActivity extends AppCompatActivity {
         (findViewById(R.id.opcion4)).setBackgroundResource(R.drawable.icon_pregunta);
         (findViewById(R.id.opcion5)).setBackgroundResource(R.drawable.icon_pregunta);
         (findViewById(R.id.opcion6)).setBackgroundResource(R.drawable.icon_pregunta);
+
+        Random random = new Random();
+        ubicacion = random.nextInt(5)+1;
+
+    }
+
+    public void destapar(int opcion){
+        if(opcion == ubicacion){
+            // Ya perdió
+            Toast.makeText(this, "¡PERMDISTE!", Toast.LENGTH_LONG).show();
+
+            for(int i =1; i<=6; i++){
+                // Iteramos cada carta (o botón)
+                ImageButton btnSeleccion = (ImageButton)  findViewById((
+                        getResources().getIdentifier("opcion"+i, "id", this.getPackageName())));
+                if(i == opcion){
+                    // Se destapa la carta mala del cheems llorando
+                    btnSeleccion.setBackgroundResource(R.drawable.icon_cheems_llora);
+                }else{
+                    // Todas las cartas restantes se destapan con el cheems normal
+                    btnSeleccion.setBackgroundResource(R.drawable.icon_cheems);
+                }
+            }
+        }else{
+            // No perdió todavía, por lo tanto se destapa solo esa carta con el cheems normal
+            ImageButton btnSeleccion = (ImageButton)  findViewById((
+                    getResources().getIdentifier("opcion"+opcion, "id", this.getPackageName())));
+        btnSeleccion.setBackgroundResource(R.drawable.icon_cheems);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.opcion1){
+            destapar(1);
+        } else if(view.getId() == R.id.opcion2){
+            destapar(2);
+        }else if(view.getId() == R.id.opcion3){
+            destapar(3);
+        }else if(view.getId() == R.id.opcion4){
+            destapar(4);
+        }else if(view.getId() == R.id.opcion5){
+            destapar(5);
+        }else if(view.getId() == R.id.opcion6){
+            destapar(6);
+        }else  if(view.getId() == R.id.btnReiniciar){
+            iniciar();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
